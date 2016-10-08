a4037bca399bd21323a6131a78844df0d06a8d0c
付会翔 <fuhuixiang@jonnyf.com>
2016-10-08 10:30:14 +0800

    add production

--- utils/get_wp_config.js
+++ utils/get_wp_config.js
@@ -32,6 +32,10 @@ module.exports = (devType)=> {
             compress: {
                 warnings: false
             }
-        })]
+        }), new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),]
     }
 };
