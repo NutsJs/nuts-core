8870870f3f08e75047c17f5f937819c657e0b9e3
付会翔 <fuhuixiang@jonnyf.com>
2016-10-13 17:06:22 +0800

    test3

---
+++ test/test-index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fuhuixiang on 16-10-13.
+ */
+"use strict";
+
+// require('./test-init');
+let init = require('./test-init');
+
+init();
+
+// console.log(init());
--- test/test-init.js
+++ test/test-init.js
@@ -8,12 +8,16 @@ const fs         = require('fs'),
       minimist   = require('minimist')(process.argv.slice(2)),
       configPath = path.resolve(process.cwd(), 'nuts.config.json');
 
-fs.exists(configPath, (exists)=> {
-    if (exists) {
-        console.log('检测到配置文件，请继续');
-    } else {
-        // console.log('未检测到配置文件，请先执行 init 命令创建配置文件');
-        throw new Error('未检测到配置文件，请先执行 init 命令创建配置文件');
-    }
-    return true;
-});
+module.exports = ()=> {
+    fs.exists(configPath, (exists)=> {
+        if (exists) {
+            console.log('检测到配置文件，请继续');
+        } else {
+            // console.log('未检测到配置文件，请先执行 init 命令创建配置文件');
+            throw new Error('未检测到配置文件，请先执行 init 命令创建配置文件');
+            // process.exit(1);
+            // return false;
+        }
+    });
+};
+
